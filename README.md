ace3fe9dd39c1a6e9f47e043c13feab1656c104a
gauravsh0812 <gauravs@arizona.edu>
2024-05-11 15:50:29 -0400

    updating

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ conda create -n endoscopic_llm python=3.9 -y ; conda activate endoscopic_llm
 ```
 
 ## Prepare dataset
+
+Re-arranging the dataset to a different format that we can use with our model.
+Need to be run intialliy only. 
 ```
-python preprocess_dataset/prepare_dataset.py
+python preprocessing/rearrange_dataset.py
+```
+
+Preprocess the image and texts to save them as tensors and questions answers, respectively. 
+```
+python preprocessing/preprocess_dataset.py
 ```
---
+++ preprocessing/preprocess_dataset.py
@@ -0,0 +1,56 @@
+import os
+import yaml
+import torch
+from box import Box
+from PIL import Image
+from torchvision import transforms
+
+# reading config file
+with open("config/config.yaml") as f:
+    cfg = Box(yaml.safe_load(f))
+
+# create an image_tensors folder
+if not os.path.exists(f"{cfg.dataset.path_to_data}/image_tensors"):
+    os.mkdir(f"{cfg.dataset.path_to_data}/image_tensors")
+if not os.path.exists(f"{cfg.dataset.path_to_data}/questions"):
+    questions = open(f"{cfg.dataset.path_to_data}/questions") 
+if not os.path.exists(f"{cfg.dataset.path_to_data}/answers"):
+    answers = open(f"{cfg.dataset.path_to_data}/answers")
+if not os.path.exists("logs"):
+        os.mkdir('logs')
+
+def preprocess(vid,img,num,qa):
+    
+    IMAGE = Image.open(f"{cfg.dataset.path_to_data}/{vid}/imgs/{img}")
+    
+    # convert to tensor
+    convert = transforms.ToTensor()
+    IMAGE = convert(IMAGE)
+
+    # saving the image 
+    torch.save(IMAGE, f"{cfg.dataset.path_to_data}/image_tensors/{num}.pt")
+
+    # wrting questions and answers
+    q = open(f"{cfg.dataset.path_to_data}/questions/question_{num}.lst") 
+    a = open(f"{cfg.dataset.path_to_data}/answers/answer_{num}.lst")
+    for _qa in qa:
+        _qas = _qa.split("|")[0]
+        _q,_a = _qas[0], _qas[1]
+        q.write(_q + "\n")
+        a.write(_a + "\n")
+
+def preprocess():
+    
+    print("creating image tensors...")
+    
+    for v in os.listdir(cfg.dataset.path_to_data):
+        if "VID" in v:
+            imgs = os.listdir(f"{cfg.dataset.path_to_data}/{v}/imgs")
+            qas = os.listdir(f"{cfg.dataset.path_to_data}/{v}/qas")
+            for im in imgs:
+                num = int(im.split(".")[0])
+                qa = os.path.join(qas, f"{num}.txt")
+                preprocess(v,im,num,qa)
+
+if __name__ == "__main__":
+    preprocess()
