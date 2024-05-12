528f4dbe7240aa7e257634dbbfae20434ada548e
gauravsh0812 <gauravs@arizona.edu>
2024-05-12 18:11:07 -0400

    update

--- models/clip.py
+++ models/clip.py
@@ -13,7 +13,7 @@ class ClipVisionEncoder(nn.Module):
         #     self.model = CLIPVisionModel(configuration)
         # else:
         self.processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
-        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
 
     def forward(self, image_paths, device):
 
