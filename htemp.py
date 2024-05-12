bc45580853f43b7af9f772b514d44acbb010cf2f
gauravsh0812 <gauravs@arizona.edu>
2024-05-12 18:17:36 -0400

    update

--- htemp.py
+++ htemp.py
@@ -1,17 +1,14 @@
 from PIL import Image
 import requests
-from transformers import AutoProcessor, CLIPVisionModel
+from transformers import AutoProcessor, CLIPVisionModelWithProjection
 
-model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+model = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-base-patch32")
 processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+# image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(images=image, return_tensors="pt")
+# inputs = processor(images=image, return_tensors="pt")
 
-outputs = model(**inputs)
-last_hidden_state = outputs.last_hidden_state
-pooled_output = outputs.pooler_output  # pooled CLS states
-
-print(last_hidden_state.shape)
+# outputs = model(**inputs)
+# image_embeds = outputs.image_embeds
