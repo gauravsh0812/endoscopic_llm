bb66bd9e0af2b68a22893c5661d9da9a1377f05f
gauravsh0812 <gauravs@arizona.edu>
2024-05-12 15:28:01 -0400

    update

--- preprocessing/preprocess_dataset.py
+++ preprocessing/preprocess_dataset.py
@@ -11,37 +11,39 @@ with open("config/config.yaml") as f:
     cfg = Box(yaml.safe_load(f))
 
 # create an image_tensors folder
-if not os.path.exists(f"{cfg.dataset.path_to_data}/image_tensors"):
-    os.mkdir(f"{cfg.dataset.path_to_data}/image_tensors")
-if not os.path.exists(f"{cfg.dataset.path_to_data}/questions"):
-    os.mkdir(f"{cfg.dataset.path_to_data}/questions") 
-if not os.path.exists(f"{cfg.dataset.path_to_data}/answers"):
-    os.mkdir(f"{cfg.dataset.path_to_data}/answers")
-if not os.path.exists("logs"):
-    os.mkdir('logs')
+if not os.path.exists(f"{cfg.dataset.path_to_data}/images"):
+    os.mkdir(f"{cfg.dataset.path_to_data}/images")
+# if not os.path.exists(f"{cfg.dataset.path_to_data}/image_tensors"):
+#     os.mkdir(f"{cfg.dataset.path_to_data}/image_tensors")
+# if not os.path.exists(f"{cfg.dataset.path_to_data}/questions"):
+#     os.mkdir(f"{cfg.dataset.path_to_data}/questions") 
+# if not os.path.exists(f"{cfg.dataset.path_to_data}/answers"):
+#     os.mkdir(f"{cfg.dataset.path_to_data}/answers")
+# if not os.path.exists("logs"):
+#     os.mkdir('logs')
 
 def preprocess_data(vid,img,num,qa):
     
-    IMAGE = Image.open(f"{cfg.dataset.path_to_data}/{vid}/imgs/{img}")
+    # IMAGE = Image.open(f"{cfg.dataset.path_to_data}/{vid}/imgs/{img}")
 
     # saving the image
     shutil.copyfile(f"{cfg.dataset.path_to_data}/{vid}/imgs/{img}",
                     f"{cfg.dataset.path_to_data}/imgs/{img}")
 
     # converting and saving to tensor
-    convert = transforms.ToTensor()
-    IMAGE = convert(IMAGE)
-    torch.save(IMAGE, f"{cfg.dataset.path_to_data}/image_tensors/{num}.pt")
+    # convert = transforms.ToTensor()
+    # IMAGE = convert(IMAGE)
+    # torch.save(IMAGE, f"{cfg.dataset.path_to_data}/image_tensors/{num}.pt")
 
-    # wrting questions and answers
-    qa = open(qa).readlines()
-    q = open(f"{cfg.dataset.path_to_data}/questions/question_{num}.lst", "w") 
-    a = open(f"{cfg.dataset.path_to_data}/answers/answer_{num}.lst", "w")
-    for _qa in qa:
-        _qas = _qa.split("|")
-        _q,_a = _qas[0], _qas[1]
-        q.write(_q + "\n")
-        a.write(_a + "\n")
+    # # wrting questions and answers
+    # qa = open(qa).readlines()
+    # q = open(f"{cfg.dataset.path_to_data}/questions/question_{num}.lst", "w") 
+    # a = open(f"{cfg.dataset.path_to_data}/answers/answer_{num}.lst", "w")
+    # for _qa in qa:
+    #     _qas = _qa.split("|")
+    #     _q,_a = _qas[0], _qas[1]
+    #     q.write(_q + "\n")
+    #     a.write(_a + "\n")
 
 def preprocess():
     
