fba7002fc4125c87912f12ee18c80a27133bcb89
gauravsh0812 <gauravs@arizona.edu>
2024-05-12 21:10:51 -0400

    update

--- models/clip.py
+++ models/clip.py
@@ -35,4 +35,4 @@ class ClipVisionEncoder(nn.Module):
         return torch.stack(_hid).to(device), torch.stack(_pool).to(device)
     
 cve = ClipVisionEncoder()
-cve(["/data/gauravs/combine_data/images/1398.png"], "cuda:0")
+cve(["/data/gauravs/combine_data/images/1398.png"], "cuda")
