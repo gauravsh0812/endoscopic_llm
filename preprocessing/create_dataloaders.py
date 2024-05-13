4a5046ac34f9559c65bced445758335bd50382bd
gauravsh0812 <gauravs@arizona.edu>
2024-05-13 16:04:33 -0400

    update

--- preprocessing/create_dataloaders.py
+++ preprocessing/create_dataloaders.py
@@ -105,9 +105,9 @@ def data_loaders(batch_size):
             _ans = open(f"{cfg.dataset.path_to_data}/answers/answer_{_idx}.lst").readlines()
             for _q,_a in zip(_qtns,_ans):
                 # keeping qtns thhat has one word answer only
-                _alist = _a.split(",")
-                if len(_alist) == 1:
-                    print(_alist[0]==" ")
+                _alist = _a.replace("\n","").strip().split(",")
+                if len(_alist) == 1 and _alist != " ":
+                    print(_alist[0])
                     ALL_QTNS.append(_q)
                     ALL_ANS.append(f"<sos> {_alist[0]} <eos>")
                     IMGS.append(f"{cfg.dataset.path_to_data}/images/{_idx}.png")
