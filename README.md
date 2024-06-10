09ec9debf61d763d6a53a401532eb773395394db
gauravsh0812 <gauravs@arizona.edu>
2024-06-10 16:59:02 -0400

    update

--- README.md
+++ README.md
@@ -21,3 +21,10 @@ Check and modify the model name as per needed.
 ```
 CUDA_VISIBLE_DEVICES=0,1,2,3 python run.py
 ```
+
+## Models
+
+#### Clip Roberta Adaptor Clf (clip_roberta_adaptor_clf)
+This model uses Clip and Roberta to encode the image and corresponding text questions. These encoded tensors are then concatenated and passed through a series of linear adaptor layers. these layers serves two important functionalities: Firstly, they are required to bring doem the higher dimensional tenors to lower one. Secondly, these are used to bring both image and text lower dimensional tensors to same domain were they will be passed through the final classification layers.
+
+#### ResNet18 Clip - Roberta GPT2 - Adaptor Clf (clip_gpt_emb_clf)
---
+++ temp.py.py
@@ -0,0 +1,26 @@
+from transformers import (
+    CLIPImageProcessor, 
+    CLIPVisionModel, 
+    RobertaModel,
+)
+from PIL import Image
+import torch
+import torch.nn as nn
+
+class ImageEncoder(nn.Module):
+    
+    def __init__(self,):
+        super(ImageEncoder, self).__init__()
+        self.processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+
+    def forward(self, image_paths, device):
+
+        for image_path in image_paths:
+            image = Image.open(image_path)
+            inputs = self.processor(images=image, return_tensors="pt").to(device)
+            outputs = self.model(**inputs)
+            print(outputs)
+
+im = ImageEncoder()
+im("/data/gauravs/surgicalGPT/our_dataset/images/1130.png")
