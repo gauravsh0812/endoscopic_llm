f92605778fd2fffe6abe8a02703d05646c8bbefc
gauravsh0812 <gauravs@arizona.edu>
2024-06-10 17:00:51 -0400

    update

--- temp.py
+++ temp.py
@@ -14,13 +14,13 @@ class ImageEncoder(nn.Module):
         self.processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
         self.model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
 
-    def forward(self, image_paths, device):
+    def forward(self, image_paths):
 
         for image_path in image_paths:
             image = Image.open(image_path)
-            inputs = self.processor(images=image, return_tensors="pt").to(device)
+            inputs = self.processor(images=image, return_tensors="pt").to("cuda:0")
             outputs = self.model(**inputs)
             print(outputs)
 
-im = ImageEncoder()
-im("/data/gauravs/surgicalGPT/our_dataset/images/1130.png")
+im = ImageEncoder().to("cuda:0")
+im(["/data/gauravs/surgicalGPT/our_dataset/images/1130.png"])
