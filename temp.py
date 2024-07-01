54d0323946a0ceb49b80307cd89d3cc4450e41ad
gauravsh0812 <gauravs@arizona.edu>
2024-07-01 16:57:00 -0400

    updating

--- temp.py
+++ temp.py
@@ -5,8 +5,8 @@ from transformers import AutoProcessor, LlavaForConditionalGeneration
 model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf")
 processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
 
-prompt = "USER: <image>\nDescribe the scene in the image? ASSISTANT:"
-image = Image.open("/data/shared/CholecT50/CholecT50/videos/VID01/001000.png")
+prompt = "USER: <image>\nElaborate the surgical image? ASSISTANT:"
+image = Image.open("/data/shared/CholecT50/CholecT50/videos/VID01/001101.png")
 
 inputs = processor(text=prompt, images=image, return_tensors="pt")
 
