28fbab951f0217a6b490b503e3fafe9df62e4d1b
gauravsh0812 <gauravs@arizona.edu>
2024-07-01 17:12:02 -0400

    updating

--- temp.py
+++ temp.py
@@ -5,12 +5,14 @@ from transformers import AutoProcessor, LlavaForConditionalGeneration
 model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf")
 processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
 
-prompt = "USER: <image>\nElaborate the surgical image? ASSISTANT:"
+_txt = "The number of tools are 2 that are grasper and hook. The phase is preparation phase and the target organ is liver. \
+    Now based on these details, elaborate this surgiccal scene."
+prompt = f"USER: <image>\n{_txt} ASSISTANT:"
 image = Image.open("/data/shared/CholecT50/CholecT50/videos/VID01/001101.png")
 
 inputs = processor(text=prompt, images=image, return_tensors="pt")
 
 # Generate
-generate_ids = model.generate(**inputs, max_new_tokens=150)
+generate_ids = model.generate(**inputs, max_new_tokens=300)
 output = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 print(output)
